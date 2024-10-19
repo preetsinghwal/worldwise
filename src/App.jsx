c19f1fbb80bc6282c62e870ba13e81c57c4f4ed9
preet singhwal <preet.singhwal@tothenew.com>
2024-10-20 02:45:57 +0530

    added some starter files, images, css, build basic architecture of project including basic components

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,19 @@ import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
-      <div>Router Paradise</div>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Homepage/>} />
           <Route path="product" element={<Product/>} />
           <Route path="pricing" element={<Pricing/>} />
+          <Route path="login" element={<Login/>} />
+          <Route path="app" element={<AppLayout/>}/>
           <Route path="*" element={<PageNotFound/>} />
         </Routes>
       </BrowserRouter>
