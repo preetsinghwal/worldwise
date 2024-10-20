1b39a7e41f251edeb410531bbc6141633bff4b49
preet singhwal <preet.singhwal@tothenew.com>
2024-10-20 23:07:04 +0530

    used useNavigate,useSearchParams hook, and handle history using nagivate and default index route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -8,6 +8,8 @@ import Login from "./pages/Login";
 import CityList from "./components/CityList";
 import { useEffect, useState } from "react";
 import CountryList from "./components/CountryList";
+import City from './components/City';
+import Form from './components/Form';
 
 const BASE_URL = 'http://localhost:8000';
 
@@ -44,10 +46,11 @@ function App() {
           <Route path="pricing" element={<Pricing />} />
           <Route path="login" element={<Login />} />
           <Route path="app" element={<AppLayout />}>
-            <Route index element={<CityList cities={cities} isLoading={isLoading} />} />
+            <Route index element={<Navigate replace to="cities"/>} />
             <Route path="cities" element={<CityList cities={cities} isLoading={isLoading} />} />
+            <Route path="cities/:id" element={<City />}/>
             <Route path="countries" element={<CountryList cities={cities} isLoading={isLoading}/>} />
-            <Route path="form" element={<p>form</p>} />
+            <Route path="form" element={<Form/>} />
           </Route>
           <Route path="*" element={<PageNotFound />} />
         </Routes>
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,8 +1,15 @@
+import { useSearchParams, useNavigate } from 'react-router-dom';
 import styles from './Map.module.css';
 
 function Map() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const lat = searchParams.get('lat');
+    const lng = searchParams.get('lng');
+    const navigate = useNavigate();
     return (
-        <div className={styles.mapContainer}>Map</div>
+        <div className={styles.mapContainer} onClick={() => navigate('form')}>Map
+        Position: {lat}, {lng}
+        </div>
     )
 }
 
